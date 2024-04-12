9f50d123424862b8660da1ca61c3f766992bcf02
Bruno Melo <brunomelohwm@gmail.com>
2024-04-12 09:53:21 -0300

    feat: add space in home-form

--- lib/widgets/home/home_form_widget.dart
+++ lib/widgets/home/home_form_widget.dart
@@ -98,6 +98,7 @@ class _HomeFormWidgetState extends State<HomeFormWidget> {
                 height: 140,
               ),
               const SizedBox(height: 20),
+              const SizedBox(height: 80),
             ],
           ),
         ),
